c39abc6ccd6ab345cc00e23f7ef9b3ee6901eecd
Brandon E <brandonelliott0594@gmail.com>
2022-07-12 21:28:39 -0600

    completed GET route

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// This is the index file. It basically has all the paths in it for the router. This project only uses one other path. That is to the notes.js file. 
+const express = require('express');
+const router = express.Router();
+const notes = require("./notes")
+
+
+
+router.use('/notes', notes)
+console.log(router)
+
+module.exports = router
---
+++ routes/notes.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+// brings in the file system module. This is for writing and appendging to files. 
+const fs = require('fs');
+// uuid for giving each note a unique ID number
+const { v4: uuidv4 } = require('uuid')
+
+// setting the database as a constant for ease of reference
+const db = "./db/db.json"
+
+
+
+// Router for a GET request to the / destination
+router.get('/', (req, res) => {
+    fs.readFile(db, "utf8", (err, response) => {
+        // Logs an error if the file doesn't exist
+        if(err) {
+            console.log(err);
+            res.status(400).send("There was an error while processing your request. Please try again")
+        // Will return the json response of the selected destination parsed out. 
+        } else{
+            console.log(JSON.parse(response));
+            res.json(JSON.parse(response));
+        }
+    })
+})
+
+// Router for a POST request to the / destination
+router.post("/", (req, res) => {
+    fs.readFile(db, "utf8", (err, response) => {
+        // logs an error if there is an error
+        if(err) {
+            console.log(err);
+            res.status(400).send("There was an error while processing your request. Please try again")
+        } else {
+            const oldNotes = JSON.parse(response)
+            // pushes the old notes with the keys of each note. uuidv gives the note a unique id. 
+            oldNotes.push({ title, text, id: uuidv4() })
+            // fs method for writing a file
+            fs.writeFile(
+                // destination of the file
+                // stringifies the old notes 
+            db, JSON.stringify(oldNotes),
+            "utf8",
+            (error) => {
+                if(error) {
+                    console.log(error)
+                }
+            })
+
+        }
+    })
+
+})
+
+
+module.exports = router
